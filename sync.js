416fc90df980131b1002779b6b77b1a686bb6617
Robert Krahn <robert.krahn@gmail.com>
2016-07-09 08:17:42 -0700

    sync: print ops / changes helpers

--- sync.js
+++ sync.js
@@ -475,3 +475,40 @@ function serializeChange(change, objectMap) {
 
   return serializedChange;
 }
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// debugging helpers
+
+function printUUID(id) {
+  if (typeof id !== "string") return String(id);
+  if (/^[a-z]/i.test(id)) return id.slice(0, id.indexOf("_")+6);
+  return id.slice(0, 5);
+}
+
+function printObj(obj) {
+  if (!obj) return String(obj);
+  if (typeof obj.serializeExpr === "function") return obj.serializeExpr();
+  if (obj.type === "lively-sync-morph-spec") return `<spec for ${printUUID(obj.spec._id)}>`
+  if (obj.type === "lively-sync-morph-ref") return `<ref for ${printUUID(obj.id)}>`
+  return lively.lang.obj.inspect(obj, {maxDepth: 1}).replace(/\n/g, "").replace(/\s\s+/g, " ");
+}
+
+function printChange(change) {
+  var {type, target, value, prop, receiver, selector, args} = change;
+  switch (type) {
+    case 'method-call':
+      return `${printUUID(receiver.id)}.${selector}(${args.map(printObj).join(",")})`;
+    case 'setter':
+      return `${printUUID(target.id)}.${prop} = ${printObj(value)}`;
+    default:
+      "?????????"
+  }
+}
+
+function printOp(op) {
+  var {id, parent, components: [change]} = op;
+  return `${printUUID(id)} < ${printUUID(parent)} | ${printChange(change)}`
+}
+
+function printOps(ops) { return ops.map(printOp).join("\n"); }
+
